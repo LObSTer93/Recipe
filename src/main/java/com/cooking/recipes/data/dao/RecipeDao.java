7ccceed1ad61e1464396686c90d8f8fd751a407b
nikita.katyshev <Nikita.Katyshev@tesco.com>
2023-12-21 22:34:55 +0100

    mongo Indexes

--- src/main/java/com/cooking/recipes/data/dao/RecipeDao.java
+++ src/main/java/com/cooking/recipes/data/dao/RecipeDao.java
@@ -1,6 +1,8 @@
 package com.cooking.recipes.data.dao;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.index.TextIndexed;
 
-public record RecipeDao(@Id String id, String name, boolean isVegetarian, int numOfServings, String ingredients, String instruction) {
+public record RecipeDao(@Id String id, @Indexed(unique = true) String name, boolean isVegetarian, int numOfServings, @TextIndexed String ingredients, @TextIndexed String instruction) {
 }
