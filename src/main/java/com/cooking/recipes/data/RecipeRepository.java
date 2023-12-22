86d849396b751b29092a0742cb89c2116ef540e7
nikita.katyshev <Nikita.Katyshev@tesco.com>
2023-12-22 08:05:42 +0100

    RecipeServiceImpl.create() impl

--- src/main/java/com/cooking/recipes/data/RecipeRepository.java
+++ src/main/java/com/cooking/recipes/data/RecipeRepository.java
@@ -2,6 +2,9 @@ package com.cooking.recipes.data;
 
 import com.cooking.recipes.data.dao.RecipeDao;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface RecipeRepository extends ReactiveMongoRepository<RecipeDao, String> {
+
+    Mono<RecipeDao> findByName(String name);
 }
